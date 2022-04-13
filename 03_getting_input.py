6b75bfe0fe2e2595b848e994f1f8b564338fff55
clayk1081 <102487708+clayk1081@users.noreply.github.com>
2022-04-13 13:03:45 +1200

    Add files via upload

---
+++ 03_getting_input.py
@@ -0,0 +1,11 @@
+# Ask the user for their name 
+username = input("what is your name?")
+# Ask the user for their favourite integer
+fav_num = int(input("Favourite Number?"))
+
+# Double, halve and square the number
+
+# Greet the user
+
+# Output the results of doubling, halving 
+# and squaring their favourite number
